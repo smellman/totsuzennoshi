d19b2cfe1c9482d200b5763715057599290901e5
Taro Matsuzawa aka. btm <btm@tech.email.ne.jp>
2012-05-06 14:54:47 +0900

    とりあえず突然の死を返すようにする

---
+++ lib/totsuzennoshi.rb
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+class Totsuzennoshi
+  def self.totsuzennoshi(message)
+    return "＿人人 人人＿\n＞ 突然の死 ＜\n￣Y^Y^Y^Y^Y￣\n"
+  end
+
+end
--- spec/totsuzennoshi_spec.rb
+++ spec/totsuzennoshi_spec.rb
@@ -1,7 +1,12 @@
+# -*- coding: utf-8 -*-
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe "Totsuzennoshi" do
-  it "fails" do
-    fail "hey buddy, you should probably rename this file and start specing for real"
+  describe "は" do
+    it "突然の死を出す" do
+      Totsuzennoshi.totsuzennoshi("突然の死").should eq("＿人人 人人＿\n＞ 突然の死 ＜\n￣Y^Y^Y^Y^Y￣\n")
+    end
+
   end
+
 end
