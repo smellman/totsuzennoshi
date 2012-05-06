04684029a3bd561da08ea617bf763ad57a09fd23
Taro Matsuzawa aka. btm <btm@tech.email.ne.jp>
2012-05-06 15:28:26 +0900

    突然の死以外にも出力できるようにした

--- lib/totsuzennoshi.rb
+++ lib/totsuzennoshi.rb
@@ -1,7 +1,36 @@
 # -*- coding: utf-8 -*-
 class Totsuzennoshi
+
+  HEADER_LR = "＿"
+  HEADER_BODY = "人"
+  HEADER_SPACE = " "
+  BODY_LEFT = "＞ "
+  BODY_RIGHT = " ＜"
+  FOOTER_LR = "￣"
+  FOOTER_BODY = "Y^"
+  FOOTER_BODY_FILL = "Y"
+
   def self.totsuzennoshi(message)
-    return "＿人人 人人＿\n＞ 突然の死 ＜\n￣Y^Y^Y^Y^Y￣\n"
+    return "#{make_header(message)}\n#{make_body(message)}\n#{make_footer(message)}\n"
+  end
+
+  def self.header_parts(message)
+    length = message.size
+    modarray = length.divmod(2)
+    return [modarray[0] + modarray[1], modarray[0]]
+  end
+
+  def self.make_footer(message)
+    return "#{FOOTER_LR}#{FOOTER_BODY*message.size}#{FOOTER_BODY_FILL}#{FOOTER_LR}"
+  end
+
+  def self.make_header(message)
+    parts = header_parts(message)
+    return "#{HEADER_LR}#{HEADER_BODY*parts[0]}#{HEADER_SPACE}#{HEADER_BODY*parts[1]}#{HEADER_LR}"
+  end
+
+  def self.make_body(message)
+    return "#{BODY_LEFT}#{message}#{BODY_RIGHT}"
   end
 
 end
--- spec/totsuzennoshi_spec.rb
+++ spec/totsuzennoshi_spec.rb
@@ -7,6 +7,50 @@ describe "Totsuzennoshi" do
       Totsuzennoshi.totsuzennoshi("突然の死").should eq("＿人人 人人＿\n＞ 突然の死 ＜\n￣Y^Y^Y^Y^Y￣\n")
     end
 
+    it "その結果がこれだよ" do
+      Totsuzennoshi.totsuzennoshi("その結果がこれだよ").should eq("＿人人人人人 人人人人＿\n＞ その結果がこれだよ ＜\n￣Y^Y^Y^Y^Y^Y^Y^Y^Y^Y￣\n")
+    end
+
+  end
+  
+  describe "ヘッダー部分の計算" do
+    it "突然の死 は配列の2, 2を返す" do
+      Totsuzennoshi.header_parts("突然の死").should == [2, 2]
+    end
+
+    it "その結果がこれだよ は配列の5, 4を返す" do
+      Totsuzennoshi.header_parts("その結果がこれだよ").should == [5, 4]
+    end
+  end
+
+  describe "フッター部分の出力" do
+    it "突然の死 は ￣Y^Y^Y^Y^Y￣ を返す" do
+      Totsuzennoshi.make_footer("突然の死").should eq("￣Y^Y^Y^Y^Y￣")
+    end
+
+    it "その結果がこれだよ は ￣Y^Y^Y^Y^Y^Y^Y^Y^Y^Y￣ を返す" do
+      Totsuzennoshi.make_footer("その結果がこれだよ").should eq("￣Y^Y^Y^Y^Y^Y^Y^Y^Y^Y￣")
+    end
+  end
+
+  describe "ヘッダー部分の出力" do
+    it "突然の死 は ＿人人 人人＿ を返す" do
+      Totsuzennoshi.make_header("突然の死").should eq("＿人人 人人＿")
+    end
+
+    it "その結果がこれだよ は ＿人人人人人 人人人人＿ を返す" do
+      Totsuzennoshi.make_header("その結果がこれだよ").should eq("＿人人人人人 人人人人＿")
+    end
+  end
+
+  describe "ボディ部分の出力" do
+    it "突然の死 は ＞ 突然の死 ＜ を返す" do
+      Totsuzennoshi.make_body("突然の死").should eq("＞ 突然の死 ＜")
+    end
+    
+    it "その結果がこれだよ は ＞ その結果がこれだよ ＜ を返す" do
+      Totsuzennoshi.make_body("その結果がこれだよ").should eq("＞ その結果がこれだよ ＜")
+    end
   end
 
 end
